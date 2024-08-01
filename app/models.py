0d0136a623d36f0eafa8ba1d561da7b7f158bf99
velmurugan9344 <113516487+velmurugan9344@users.noreply.github.com>
2024-08-01 22:15:31 +0530

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,120 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+STATE_CHOICES= (
+    ('Andhra Pradesh', 'Andhra Pradesh'),
+    ('Arunachal Pradesh', 'Arunachal Pradesh'),
+    ('Assam', 'Assam'),
+    ('Bihar', 'Bihar'),
+    ('Chhattisgarh', 'Chhattisgarh'),
+    ('Goa', 'Goa'),
+    ('Gujarat', 'Gujarat'),
+    ('Haryana', 'Haryana'),
+    ('Himachal Pradesh', 'Himachal Pradesh'),
+    ('Jharkhand', 'Jharkhand'),
+    ('Karnataka', 'Karnataka'),
+    ('Kerala', 'Kerala'),
+    ('Madhya Pradesh', 'Madhya Pradesh'),
+    ('Maharashtra', 'Maharashtra'),
+    ('Manipur', 'Manipur'),
+    ('Meghalaya', 'Meghalaya'),
+    ('Mizoram', 'Mizoram'),
+    ('Nagaland', 'Nagaland'),
+    ('Odisha', 'Odisha'),
+    ('Punjab', 'Punjab'),
+    ('Rajasthan', 'Rajasthan'),
+    ('Sikkim', 'Sikkim'),
+    ('Tamil Nadu', 'Tamil Nadu'),
+    ('Telangana', 'Telangana'),
+    ('Tripura', 'Tripura'),
+    ('Uttar Pradesh', 'Uttar Pradesh'),
+    ('Uttarakhand', 'Uttarakhand'),
+    ('West Bengal', 'West Bengal'),
+    ('Andaman and Nicobar Islands', 'Andaman and Nicobar Islands'),
+    ('Chandigarh', 'Chandigarh'),
+    ('Dadra and Nagar Haveli and Daman and Diu', 'Dadra and Nagar Haveli and Daman and Diu'),
+    ('Lakshadweep', 'Lakshadweep')
+ )
+
+
+CATEGORY_CHOICES=(
+    ('GH','Ghee'),
+    ('CR','Curd'),
+    ('MS','Milkshake'),
+    ('LS','Lassi'),
+    ('ML','Milk'),
+    ('PN','Paneer'),
+    ('CZ','Cheese'),
+    ('IC','Ice-creams'),
+    )
+class Product(models.Model):
+    title=models.CharField(max_length=100)
+    selling_price=models.FloatField()
+    discounted_price=models.FloatField()
+    description=models.TextField()
+    composition=models.TextField(default='')
+    prodapp=models.TextField(default='')
+    category=models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    product_image=models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
+    
+    def formatted_selling_price(self):
+        return f"₹{self.selling_price:.2f}"
+
+    def formatted_discounted_price(self):
+        return f"₹{self.discounted_price:.2f}"
+    
+
+class Customer(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=200)
+    locality=models.CharField(max_length=200)
+    city=models.CharField(max_length=50)
+    mobile=models.IntegerField(default=0)
+    zipcode=models.IntegerField()
+    state=models.CharField(choices=STATE_CHOICES,max_length=100)
+    def __str__(self):
+        return self.name
+    
+
+class Cart(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+
+    @property
+    def total_cost(self):
+        return self.quantity*self.product.discounted_price
+    
+
+STATUS_CHOICES=(
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The Way','On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel'),
+    
+    ('Pending','Pending'),
+)
+
+class Payment(models.Model):
+     user=models.ForeignKey(User,on_delete=models.CASCADE)
+     amount=models.FloatField()
+     razorpay_order_id=models.CharField(max_length=100,blank=True,null=True)
+     razorpay_payment_status=models.CharField(max_length=100,blank=True,null=True)
+     razorpay_payment_id=models.CharField(max_length=100,blank=True,null=True)
+     paid=models.BooleanField(default=False)
+
+class OrderPlaced(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    Customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    Product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    Quantity=models.PositiveIntegerField(default=1)
+    Ordered_date=models.DateTimeField(auto_now_add=True)
+    Status=models.CharField(max_length=50,choices=STATUS_CHOICES,default='Pending')
+    Payment=models.ForeignKey(Payment,on_delete=models.CASCADE,default="")
+    @property
+    def total_cose(self):
+        return self.quantity * self.Product.discounted_Price
